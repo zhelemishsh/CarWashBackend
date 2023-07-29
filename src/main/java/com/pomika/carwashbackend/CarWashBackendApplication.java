ae7b8f576e8ff17b1de7618563f014bbfe89e0b1
egosh <egoshin.deniska@mail.ru>
2023-07-30 00:40:09 +0300

    feat: add dependencies

---
+++ src/main/java/com/pomika/carwashbackend/CarWashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pomika.carwashbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWashBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarWashBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pomika/carwashbackend/CarWashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomika.carwashbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
